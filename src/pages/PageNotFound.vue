844a170fde826776bd418cb612320523004517fe
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 18:47:29 +0700

    change style to module

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="col-full">
-    <h1>Not found</h1>
-    <p>Oooops, we couldn't find what you are looking for. Why don't you 
+  <div class="col-full" :class="$style['centered']">
+    <h1 :class="$style['heading-large']" >Not found</h1>
+    <p :class="$style['text-large']" >Oooops, we couldn't find what you are looking for. Why don't you 
       <router-link :to="{name: 'Home'}">go home instead?</router-link>
     </p>
   </div>
@@ -13,16 +13,16 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
+<style module>
+.heading-large {
   font-size: 100px;
 }
 
-p {
+.text-large {
   font-size: 50px;
   font-weight: 100;
 }
-div {
+.centered {
   text-align: center;
 }
 </style>

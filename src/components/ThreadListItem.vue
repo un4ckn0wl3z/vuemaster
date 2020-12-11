9383a5537b3646069ac1a7b0972ffd9e33d3ae5e
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 22:16:19 +0700

    add AppDate base component

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -5,7 +5,7 @@
                           <router-link :to="{name: 'ThreadShow', params: {id: thread['.key']}}">{{thread.title}}</router-link>
                       </p>
                       <p class="text-faded text-xsmall">
-                          By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
+                          By <a href="#">{{user.name}}</a>, <AppDate :timestamp="thread.publishedAt"/>.
                       </p>
                   </div>
 
@@ -28,8 +28,12 @@
 
 <script>
 import sourceData from '@/data'
+import AppDate from './AppDate'
 
 export default {
+  components: {
+    AppDate
+  },
   props: {
     thread: {
       required: true,

e1e088f1f3954f1d6aff6a6d14c4bdf8ef2766ce
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 16:51:58 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div
+      v-for="(thread, index) in threads"
+      :key="index"
+      class="col-large push-top"
+    >
+      <h1>{{ thread.title }}</h1>
+      <div class="post-list">
+        <div
+          v-for="(postId, index) in thread.posts"
+          :key="index"
+          class="post"
+        >
+          <div class="user-info">
+            <a
+              href="#"
+              class="user-name"
+            >{{
+              users[posts[postId].userId].name
+            }}</a>
+            <a href="#">
+              <img
+                class="avatar-large"
+                :src="users[posts[postId].userId].avatar"
+                alt=""
+              />
+            </a>
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+          <div class="post-content">
+            <div>
+              {{ posts[postId].text }}
+            </div>
+          </div>
+          <div class="post-date text-faded">
+            {{ posts[postId].publishedAt }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+console.log(sourceData)
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>

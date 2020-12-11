ec84f6f9d77e877e9028faebb97eebccc62caa0e
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 22:27:52 +0700

    fix typo

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import AppDate from './components/AppDate.vue'
+import AppDate from '@/components/AppDate.vue'
 
 Vue.component('AppDate', AppDate)
 

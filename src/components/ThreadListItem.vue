db82cd0e34527c63a2ce10525fd6c2c31de1a914
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 22:27:15 +0700

    move AppDate to Global Component

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -28,12 +28,8 @@
 
 <script>
 import sourceData from '@/data'
-import AppDate from './AppDate'
 
 export default {
-  components: {
-    AppDate
-  },
   props: {
     thread: {
       required: true,

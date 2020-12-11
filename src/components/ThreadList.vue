bb6d74f0438677d9e77cdd6d59e84974e169a09e
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 17:49:19 +0700

    develop main forum page

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="thread-list">
+		<h2 class="list-title">Threads</h2>
+		<ThreadListItem v-for="(thread,index) in threads" :key="index" :thread="thread"/>
+	</div>
+</template>
+
+<script>
+
+import ThreadListItem from './ThreadListItem'
+
+export default {
+  components: {
+    ThreadListItem
+  },
+  props: {
+    threads: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>

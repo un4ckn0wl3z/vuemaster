6c4744f528f5e1b62806786630fef63ffa85fc24
un4ckn0wl3z <slickerth@gmail.com>
2020-12-11 19:21:15 +0700

    refactor post components

---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+      <div class="post-list">
+        <PostListItem v-for="(post,index) in posts" :key="index" :post="post"/>
+      </div>
+</template>
+
+<script>
+import PostListItem from './PostListItem'
+export default {
+  components: {
+    PostListItem
+  },
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>

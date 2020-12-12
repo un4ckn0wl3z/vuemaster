a04326290dc54dba09306ec60ac942062dfb9173
un4ckn0wl3z <slickerth@gmail.com>
2020-12-12 14:10:39 +0700

    add forum pages

---
+++ src/pages/PageForum.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+      <div class="col-full push-top">
+          <div class="forum-header">
+              <div class="forum-details">
+                  <h1>Cooking</h1>
+                  <p class="text-lead">Discuss your passion for food and cooking</p>
+              </div>
+              <a href="new-thread.html" class="btn-green btn-small">Start a thread</a>
+          </div>
+      </div>
+      <div class="col-full">
+        <ThreadList :threads="threads"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import ThreadList from '@/components/ThreadList'
+import sourceData from '@/data'
+
+export default {
+  components: {
+    ThreadList
+  },
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  computed: {
+    forum () {
+      return sourceData.forums[this.id]
+    },
+    threads () {
+      return Object.values(sourceData.threads).filter(thread => thread.forumId === this.id)
+    }
+  }
+
+}
+</script>
